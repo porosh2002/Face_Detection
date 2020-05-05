8559624e258e0d055f9fca41d74fe79bbca14513
porosh2002 <jamilkashem2002@gmail.com>
2020-05-05 12:48:16 +0600

    Updates

---
+++ precache-manifest.23950a24b4cfccfdefb13262fffcae0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5deb3e26355818c7026a6ef180e11dc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dec507fb1ed4ede78e4",
+    "url": "/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "6e21f1bcf8b9bea2316c",
+    "url": "/static/css/main.93bbc702.chunk.css"
+  },
+  {
+    "revision": "4dec507fb1ed4ede78e4",
+    "url": "/static/js/2.f40c5c3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f40c5c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e21f1bcf8b9bea2316c",
+    "url": "/static/js/main.8eab8639.chunk.js"
+  },
+  {
+    "revision": "97f6caadecd1f2c3c7a3",
+    "url": "/static/js/runtime-main.13a1d77f.js"
+  },
+  {
+    "revision": "d01731eecbeb065d79d807dacb623a93",
+    "url": "/static/media/bg-inpt.d01731ee.png"
+  }
+]);
